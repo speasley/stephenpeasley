afd4234dc6b73d8fd25132563dab7ecf34dc69a3
speasley <s@stephenpeasley.com>
2022-07-15 22:26:41 -0600

    feat: use vars for colours

--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -33,13 +33,6 @@ i {
   color: var(--color-text);
 }
 
-h3 {
-  font-size: 1.2rem;
-  font-weight: 500;
-  margin-bottom: 0.4rem;
-  color: var(--color-heading);
-}
-
 @media (min-width: 1024px) {
   .item {
     margin-top: 0;
