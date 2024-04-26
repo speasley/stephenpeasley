c48764517092b61c6f85e30fa3dff0bc387aab86
Stephen Peasley <speasley@users.noreply.github.com>
2024-04-26 09:26:30 -0600

    feat: move RRU paragraph to end

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -11,6 +11,14 @@ import SImage from "@/components/SImage.vue";
       threat. I live in Mohkinstsis/Calgary.
     </p>
     <p>
+      I was born and raised in Saskatchewan, on the mean streets of Humboldt. I
+      shifted west in 2007. I love collaborating with other creative and
+      passionate people. I value early collaboration, brainstorming, and a
+      thoughtful approach. I also value good humour, good crosswords, and good
+      health.
+    </p>
+    <p>I aim to leave things better than how I found them.</p>
+    <p>
       I am currently pursuing a
       <a
         href="https://www.royalroads.ca/programs/master-arts-learning-and-technology"
@@ -20,13 +28,5 @@ import SImage from "@/components/SImage.vue";
       you would like to follow my academic writings, you can find them on my
       <a href="https://malat-webspace.royalroads.ca/rru297/">blog</a>.
     </p>
-    <p>
-      I was born and raised in Saskatchewan, on the mean streets of Humboldt. I
-      shifted west in 2007. I love collaborating with other creative and
-      passionate people. I value early collaboration, brainstorming, and a
-      thoughtful approach. I also value good humour, good crosswords, and good
-      health.
-    </p>
-    <p>I aim to leave things better than how I found them.</p>
   </article>
 </template>
